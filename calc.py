cf68dbc2dab4128cfb8f9523071fe5bf7aeb87f0
diwadu <diwadu@debian12vm.debian12vm>
2024-02-04 18:45:19 +0100

    calc.py added

---
+++ calc.py
@@ -0,0 +1,12 @@
+import sys
+
+def sum(x, y):
+    return x + y
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <number1> <number2>")
+        sys.exit(1)
+
+    result = sum(int(sys.argv[1]), int(sys.argv[2]))
+    print(result)
