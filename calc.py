29081e53c4f8b698c2d978bcad5cee08f260d153
diwadu <diwadu@debian12vm.debian12vm>
2024-02-04 19:05:55 +0100

    Added dot

--- calc.py
+++ calc.py
@@ -5,7 +5,7 @@ def sum(x, y):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: python script.py <number1> <number2>")
+        print("Usage: python script.py <number1> <number2>.")
         sys.exit(1)
 
     result = sum(int(sys.argv[1]), int(sys.argv[2]))
